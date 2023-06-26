0b95150e97c6b9d2e350463f036fbed61fe1c02e
lyrawb@gmail.com <lyrawb@gmail.com>
2023-06-26 20:14:19 +0600

    lesson6

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import css from './app.module.css'
+
 
 function App() {
+  const [count,setCount] = useState(0)
+const [count2,setCount2] = useState(0)
+  const asds = (count)=>{
+    if(count>10){
+      const divs= Array.from({length:10},()=>{
+      return(
+        <div style={{width:'250px',height:'250px',border:'1px solid black'}}>
+
+        </div>
+      )
+    })
+
+    return divs
+    }else{
+      const divs= Array.from({length:count},()=>{
+        return(
+          <div style={{width:'250px',height:'250px',border:'1px solid black'}}>
+  
+          </div>
+        )
+      })
+  
+      return divs
+    }
+   
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+   <input type="text" onChange={(e)=>{setCount(e.target.value)}}/>
+   <button onClick={()=>setCount2(count)}>  create</button>
+   {
+    asds(count2)
+   }
     </div>
   );
 }
